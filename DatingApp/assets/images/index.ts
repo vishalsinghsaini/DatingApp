ff2ea7324e783b522d8ec3e7a69708207f2ca27b
Vishal Singh Saini <vishalsinghsaini@Vishals-MacBook-Air.local>
2024-09-02 17:19:54 +0530

    Dating app changes 2

---
+++ DatingApp/assets/images/index.ts
@@ -0,0 +1,78 @@
+import karinaTessProfilePic from './karinaTessProfilePic.png';
+import karinaTessProfilePic1 from './karinaTessProfilePic1.png';
+import karinaTessProfilePic2 from './karinaTessProfilePic2.png';
+import karinaTessImage from './karinaTessImage.svg';
+import karinaTessImage1 from './karinaTessImage1.svg';
+import karinaTessImage2 from './karinaTessImage2.svg';
+import BeerMugs from './BeerMugs.png';
+import Cigarette from './Cigarette.png';
+import Headphone from './Headphone.png';
+import Notebook from './Notebook.png';
+import Om from './Om.png';
+import Taurus from './Taurus.png';
+import Weights from './Weights.png';
+import Women from './Women.png';
+import HighVoltage from './HighVoltage.png';
+import Pushpin from './Pushpin.png';
+
+import Heart from './Heart.png';
+import Thread from './Thread.png';
+import PurpleStar from './PurpleStar.png';
+import GreenStar from './GreenStar.png';
+import PinkStar from './PinkStar.png';
+import Girl from './Girl.png';
+import Boy from './Boy.png';
+
+
+
+export const images = {
+    karinaTessProfilePic,
+    karinaTessProfilePic1,
+    karinaTessProfilePic2,
+    karinaTessImage,
+    karinaTessImage1,
+    karinaTessImage2,
+    BeerMugs,
+    Cigarette,
+    Headphone,
+    Notebook,
+    Om,
+    Taurus,
+    Weights,
+    Women,
+    HighVoltage,
+    Pushpin,
+
+    Heart,
+    Thread,
+    PurpleStar,
+    GreenStar,
+    PinkStar,
+    Girl,
+    Boy,
+}
+
+export { default as karinaTessProfilePic } from './karinaTessProfilePic.png';
+export { default as karinaTessProfilePic1 } from './karinaTessProfilePic1.png';
+export { default as karinaTessProfilePic2 } from './karinaTessProfilePic2.png';
+export { default as karinaTessImage } from './karinaTessImage.svg';
+export { default as karinaTessImage1 } from './karinaTessImage1.svg';
+export { default as karinaTessImage2 } from './karinaTessImage2.svg';
+export { default as BeerMugs } from './BeerMugs.png';
+export { default as Cigarette } from './Cigarette.png';
+export { default as Headphone } from './Headphone.png';
+export { default as Notebook } from './Notebook.png';
+export { default as Om } from './Om.png';
+export { default as Taurus } from './Taurus.png';
+export { default as Weights } from './Weights.png';
+export { default as Women } from './Women.png';
+export { default as HighVoltage } from './HighVoltage.png';
+export { default as Pushpin } from './Pushpin.png';
+
+export { default as Heart } from './Heart.png';
+export { default as Thread } from './Thread.png';
+export { default as PurpleStar } from './PurpleStar.png';
+export { default as GreenStar } from './GreenStar.png';
+export { default as PinkStar } from './PinkStar.png';
+export { default as Girl } from './Girl.png';
+export { default as Boy } from './Boy.png';
