c987a70ebc7edd0b1c7e5c96be11a34b22301234
Vishal Singh Saini <vishalsinghsaini@Vishals-MacBook-Air.local>
2024-09-02 18:09:00 +0530

    dating app changes 4

--- DatingApp/App.tsx
+++ DatingApp/App.tsx
@@ -11,6 +11,7 @@ const Stack = createNativeStackNavigator();
 const ProfileScreen = () => {
   const { width, height } = useWindowDimensions();
   const scrollX = useRef(new Animated.Value(0)).current;
+  const navigation = useNavigation()
   const animatedCurrent = useRef(Animated.divide(scrollX, width)).current;
   const imageArray = [
     {
@@ -109,63 +110,56 @@ const ProfileScreen = () => {
     )
   }
   return (
-    <>
+    <SafeAreaView>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <Animated.ScrollView
-            horizontal={true}
-            pagingEnabled={true}
-            style={{
-              // width: 376,
-              height: 382,
-              borderRadius: 22,
-              marginTop: 10,
-              // marginHorizontal: 16
-            }}
-            showsHorizontalScrollIndicator={false}
-            onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
-              useNativeDriver: true,
-            })}
-          >
-            {pages.map((page, index) => (
-              <View key={index} style={[styles.page, { width, height }]}>
-                <ImageBackground
-                  source={imageArray?.[index]?.uri}
-                  style={{
-                    width: 376,
-                    height: 382
-                  }}
-                >
-                  <View style={styles.pageIndicator}>
-                    <PageIndicator count={pages.length} current={animatedCurrent} variant={'train'} color={'white'} />
-                  </View>
+        <Animated.ScrollView
+          horizontal={true}
+          pagingEnabled={true}
+          scrollToOverflowEnabled
+          contentContainerStyle={{
+            height: 382,
+            borderRadius: 50
+          }}
+          showsHorizontalScrollIndicator={false}
+          onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
+            useNativeDriver: true,
+          })}
+        >
+          {pages.map((page, index) => (
+            <View key={index} style={[{ width: width, height, paddingHorizontal: 16 }]}>
+              <ImageBackground
+                source={imageArray?.[index]?.uri}
+                style={{
+                  height: 382,
+                  borderRadius: 22,
+                  paddingHorizontal: 16
+                }}
+              >
+                <PageIndicator count={pages.length} current={animatedCurrent} variant={'train'} color={'white'} style={{ top: 15 }} dashSize={10} />
+                <View style={{ flexDirection: 'row', bottom: 20, position: 'absolute', marginLeft: 16 }}>
+                  {flatListData.map((item, index) => (
+                    <GlassView glassStyle={{
+                      paddingHorizontal: 12, paddingVertical: 8,
+                      borderRadius: 16, marginRight: index !== flatListData.length - 1 ? 9 : 0, flexDirection: 'row', alignItems: 'center'
+                    }}>
+                      {flatListData?.[index]?.uri && <ImageBackground
+                        source={flatListData?.[index]?.uri}
+                        style={{
+                          width: 18,
+                          height: 18
+                        }}
+                      />}
+                      <Text style={{ color: 'white', fontWeight: '600', textAlign: 'center' }}>
+                        {item.label}
+                      </Text>
+                    </GlassView>
+                  ))}
+                </View>
 
-                  <View style={{ flexDirection: 'row', top: 340, marginLeft: 17 }}>
-                    {flatListData.map((item, index) => (
-                      <GlassView glassStyle={{
-                        paddingHorizontal: 12, paddingVertical: 8,
-                        borderRadius: 16, marginRight: 9, flexDirection: 'row', alignItems: 'center'
-                      }}>
-                        {flatListData?.[index]?.uri && <ImageBackground
-                          source={flatListData?.[index]?.uri}
-                          style={{
-                            width: 18,
-                            height: 18
-                          }}
-                        />}
-                        <Text style={{ color: 'white', fontWeight: '600', textAlign: 'center' }}>
-                          {item.label}
-                        </Text>
-                      </GlassView>
-                    ))}
-                  </View>
-
-                </ImageBackground>
-              </View>
-            ))}
-
-          </Animated.ScrollView>
-        </View>
+              </ImageBackground>
+            </View>
+          ))}
+        </Animated.ScrollView>
 
         <View style={[styles.common, styles.shadowProp]}>
           <View>
@@ -207,13 +201,14 @@ const ProfileScreen = () => {
       </ScrollView>
       <TouchableOpacity
         activeOpacity={1}
+        onPress={() => navigation.goBack()}
         style={{ marginHorizontal: 57, borderRadius: 10, paddingHorizontal: 74, paddingVertical: 16, height: 50, backgroundColor: '#736CD3', justifyContent: 'center', alignSelf: 'center', position: 'absolute', bottom: 10 }}
       >
         <Text style={{ color: 'white', fontWeight: '600' }}>
           Send Message
         </Text>
       </TouchableOpacity>
-    </>
+    </SafeAreaView>
   )
 }
 
@@ -317,6 +312,7 @@ const App = () => {
 const styles = StyleSheet.create({
   pageIndicator: {
     top: 20,
+    borderWidth: 5,
     width: '100%',
     position: 'absolute',
     alignItems: 'center',
